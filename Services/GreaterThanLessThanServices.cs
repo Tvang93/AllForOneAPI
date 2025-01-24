98dd192c72e6ebc073cda6bb5d3b014ccb872d8f
Tvang93 <vangt39@gmail.com>
2025-01-24 12:18:52 -0800

    updated

--- Services/GreaterThanLessThanServices.cs
+++ Services/GreaterThanLessThanServices.cs
@@ -12,16 +12,16 @@ namespace AllForOneAPI.Services
         public string GreaterThanLessThan(int num1, int num2)
         {
             if(num1 == num2){
-                firstStatement = $"Your first number: {num1} is equal to your second number: {num2}.";
-                secondStatement = $"Your second number: {num2} is equal to your first number: {num1}.";
+                firstStatement = $"Your first number '{num1}' is equal to your second number '{num2}'.";
+                secondStatement = $"Your second number '{num2}' is equal to your first number '{num1}'.";
                 return firstStatement + secondStatement;
             }else if(num1 > num2){
-                firstStatement = $"Your first number: {num1} is greater than your second number: {num2}.";
-                secondStatement = $"Your second number: {num2} is less than your first number: {num1}.";
+                firstStatement = $"Your first number '{num1}' is greater than your second number '{num2}'.";
+                secondStatement = $"Your second number '{num2}' is less than your first number '{num1}'.";
                 return firstStatement + secondStatement;
             }else{
-                firstStatement = $"Your first number: {num1} is less than your second number: {num2}.";
-                secondStatement = $"Your second number: {num2} is greater than your first number: {num1}.";
+                firstStatement = $"Your first number '{num1}' is less than your second number '{num2}'.";
+                secondStatement = $"Your second number '{num2}' is greater than your first number '{num1}'.";
                 return firstStatement + secondStatement;
             }
         }
